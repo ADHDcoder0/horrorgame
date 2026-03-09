3b4dd21b453973fe40d96de5316bd2ae4307e6e1
7IRE <lm3419631@gmail.com>
2026-03-09 22:56:53 +0530

    World & Room Generator

--- src/world/Room.h
+++ src/world/Room.h
@@ -15,6 +15,7 @@ enum class TileType {
 struct Tile{  // a single tile 
     TileType type =TileType::Floor;
     bool visible = true;
+    bool isMovable = false;
 };
 
 struct TaskItem{
@@ -22,20 +23,22 @@ struct TaskItem{
     std::string name;//we need to expand this further 
 };
 
+
+
 class Room {
     public:
         int id;
-        int gridX,gridY;
-        int floor;
-        int worldOffsetX;
-        int worldOffsetY;
-        float centerX,CentreY;
+        int Xsize,Ysize;
 
-        std::vector<std::vector<Tile>> tiles;
+        Tile Wall;
+        Tile Empty;
+        Tile Floor;
+        
+        std::vector<std::vector<Tile*>> tiles{17, std::vector<Tile*>(17, nullptr)};
         std::vector<TaskItem> items;
 
         //constructor
-        Room(int id, int gridX, int gridY, int w, int h, int floor, int offsetX = 0, int offsetY = 0);
+        Room(int Xsize , int Ysize , bool LeftGate, bool RightGate , bool TopGate , bool BottomGate);//Xsize and Ysize <= 17 blocks 
 
         void Update();
         void Draw(int tileSize);
@@ -48,4 +51,32 @@ class Room {
   
         void GenerateItem(const std::string& itemName);  // gives the bound of room in  pixels
 
-    };
+};
+
+// class Room {
+//     public:
+//         int id;
+//         int gridX,gridY;
+//         int floor;
+//         int worldOffsetX;
+//         int worldOffsetY;
+//         float centerX,CentreY;
+
+//         std::vector<std::vector<Tile *>> tiles;
+//         std::vector<TaskItem> items;
+
+//         //constructor
+//         Room(int id, int gridX, int gridY, int w, int h, int floor, int offsetX = 0, int offsetY = 0);
+
+//         void Update();
+//         void Draw(int tileSize);
+//         void Generate();  //-> for assigning floor and border 
+        
+//         bool isWalkable(int tx,int ty) const; // -> return if you canwalk in the grid or not 
+
+//         Rectangle GetBounds(int tileSize) const;
+
+  
+//         void GenerateItem(const std::string& itemName);  // gives the bound of room in  pixels
+
+//     };
